009af579ff444fa1bbf5d20fb0c33fe230f2c357
andela-eashikodi <emeka.ashikodi@andela.co>
2015-05-10 23:11:37 +0100

    edit schema

--- app/models/student.model.js
+++ app/models/student.model.js
@@ -3,10 +3,25 @@
 var mongoose = require('mongoose');
 
 var regSchema = new mongoose.Schema({
-  firstname:String,
-  lastname:String,
-  gender: String,
-  state:String,
-  dob:Date
+  firstname: {
+    type: String,
+    required : 'enter firstname'
+  },
+  lastname: {
+    type: String,
+    required : 'enter lastname'
+  },
+  gender: {
+    type: String,
+    required: 'Enter Gender'
+  },
+  state: {
+    type: String,
+    required : 'enter state of origin'
+  },
+  dob: {
+    type: Date,
+    required : 'enter date of birth'
+  }
 });
 mongoose.model('Student', regSchema);
