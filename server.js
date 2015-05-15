2c502efd7f47759f0fbf6a2bea52626ca7f1aa64
andela-eashikodi <emeka.ashikodi@andela.co>
2015-05-15 14:37:54 +0100

    middlewares

--- server.js
+++ server.js
@@ -18,6 +18,13 @@ mongoose.connect(config[process.env.NODE_ENV]['url']);
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
+
 app.get('/', function(req, res){
   res.send('welcome to student enrollment system');
 });
