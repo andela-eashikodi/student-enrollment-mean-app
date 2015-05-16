27b33477580fecd1b959e5d2b97a52c8026e293f
andela-eashikodi <emeka.ashikodi@andela.co>
2015-05-16 15:30:13 +0100

    added http cors

--- server.js
+++ server.js
@@ -16,11 +16,12 @@ app.use(morgan('dev'));
 var mongoose = require('mongoose');
 mongoose.connect(config[process.env.NODE_ENV]['url']);
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
